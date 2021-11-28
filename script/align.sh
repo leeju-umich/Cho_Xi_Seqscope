67d2e08c429cd6b0eed3472ffd8f51c71d77b2e7
jyxi7676 <49928713+jyxi7676@users.noreply.github.com>
2021-11-28 18:56:46 -0500

    fix bug

--- script/align.sh
+++ script/align.sh
@@ -24,7 +24,7 @@ then
 else 
 	paste <(zcat ./file1_trim.fastq.gz) <(zcat $file2) | perl -lane 'if ( $. % 4 == 1 ) { print "$F[0] $F[1]"; } elsif ( $. % 4 == 3 ) { print "+"; } else { print substr($F[0],0,20).substr($F[1],0,9).substr($F[0],50); }' > $file1_final
 
-
+fi
 #paste <(zcat $file1) <(zcat $file2) | perl -lane 'if ( $. % 4 == 1 ) { print "$F[0] $F[1]"; } elsif ( $. % 4 == 3 ) { print "+"; } else { print substr($F[0],0,30).substr($F[1],0,9).substr($F[0],50); }' > $file1_final
 #paste <(zcat $file1_trim) <(zcat $file2) | perl -lane 'if ( $. % 4 == 1 ) { print "$F[0] $F[1]"; } elsif ( $. % 4 == 3 ) { print "+"; } else { print substr($F[0],0,30).substr($F[1],0,9).substr($F[0],50); }' > $file1_final_fq
 
